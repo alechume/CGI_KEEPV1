b9bfa34a576315a32c7693a44c87fe2508d78e1e
alechume <alec.lawson.hume@gmail.com>
2020-11-03 14:40:22 -0700

    Some changes

--- KEEPApp/src/keepapi/urls.py
+++ KEEPApp/src/keepapi/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('model/', include('keep.urls')),
+    path('keepapp/model/', include('keep.urls')),
 ]
